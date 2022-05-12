1ea7b6b452767f0a042d7577aa25add2f9138394
Phoebe <100450610+phoebe0909@users.noreply.github.com>
2022-05-11 19:23:39 -0700

    Update Branch.java
    Rounding transactions to two decimal places. Cleaning up print statements.

--- Branch.java
+++ Branch.java
@@ -1,16 +1,16 @@
 package com.company;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Branch {
-
-    private String branchName;
-    private ArrayList<Customer> myCustomers;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    private String name;
+    private ArrayList<Customer> customers;
 
     public Branch(String branchName, ArrayList<Customer> myCustomers) {
-        this.myCustomers = myCustomers;
-        this.branchName = branchName;
+        this.customers = myCustomers;
+        this.name = branchName;
     }
 
     public static Branch createBranch(String branchName) {
@@ -18,83 +18,49 @@ public class Branch {
         return new Branch(branchName, myCustomers);
     }
 
-    public String getBranchName() {
-        return branchName;
+    public String getName() {
+        return name;
     }
 
-    public ArrayList<Customer> getMyCustomers() {
-        return myCustomers;
+    public ArrayList<Customer> getCustomers() {
+        return customers;
     }
 
 
-    public int findCustomer(String customerName){
-        for (int i=0; i<this.myCustomers.size(); i++) {
-            Customer customer = this.myCustomers.get(i);
-            if (customer.getCustomerName().equals(customerName)) {
-                return i;
+    private Customer findCustomer(String customerName){
+        for (int i = 0; i<this.customers.size(); i++) {
+            Customer customer = this.customers.get(i);
+            if (customer.getName().equals(customerName)) {
+                return customer;
             }
         }
-        return -1;
+        return null;
     }
 
 
-    public boolean addCustomer(String customerName, double startingBalance) {
-        if (findCustomer(customerName)>=0){
+    public boolean newCustomer(String customerName, double startingBalance) {
+        if (findCustomer(customerName)!=null){
             System.out.println("Customer is already in system.");
             return false;
         }
         else {
-            this.myCustomers.add(Customer.createCustomer(customerName,
-                    startingBalance));
+            Customer customer = new Customer(customerName, startingBalance);
+            this.customers.add(customer);
             System.out.println("Customer " + customerName + " added.");
             return true;
         }
     }
 
-
-
-    public void printTransactions(String name){
-        int index =  findCustomer(name);
-        System.out.println("Printing transactions:");
-        if (index <0){
-            System.out.println("Customer name invalid");
-        }
-        else{ //This will have to be printed in a different format.
-        Customer customer = myCustomers.get(index);
-        System.out.println(customer.getTransactions());
-        }
-    }
-
-    public void addTransaction(double deposit, String name){
+    public boolean addCustomerTransaction(double deposit, String name){
         //overloading method in customer. This is called first, then passed
         // to the customer version.
-        int index = findCustomer(name);
-        if (index>=0){
-            Customer customer = (myCustomers.get(index));
-            customer.addTransaction(deposit);
+        if (findCustomer(name)==null){
+            System.out.println("Customer name invalid.");
+            return false;
         }
         else{
-            System.out.println("Customer name invalid.");
-        }
-
-    }
-
-    public void printCustomers() {
-        System.out.println("List of customers:");
-        for (int i = 0; i< myCustomers.size(); i++) {
-            Customer customer = myCustomers.get(i);
-            customer.getCustomerName();
-            System.out.println((i+1) + ". " + customer.getCustomerName());
-        }
-    }
-
-    public void printCustomersAndTransactions(){
-        System.out.println("List of customers:");
-        for (int i = 0; i< myCustomers.size(); i++) {
-            Customer customer = myCustomers.get(i);
-
-            System.out.println((i+1) + ". " + customer.getCustomerName());
-            printTransactions(customer.getCustomerName());
+            findCustomer(name).addTransaction(deposit);
+            return true;
         }
     }
 
