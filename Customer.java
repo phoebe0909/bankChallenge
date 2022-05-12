69a7f83ae665140f60836c9eaf9a8a977f43f7b2
Phoebe <100450610+phoebe0909@users.noreply.github.com>
2022-05-11 19:24:38 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,13 +1,13 @@
 package com.company;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Customer {
 
-
-    private String customerName;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    private String name;
     private ArrayList<Double> transactions;
-    private double startingBalance;
     private Branch myBranch;
 
 
@@ -15,22 +15,12 @@ public class Customer {
     public Customer(String customerName,
                     double startingBalance) {
         this.transactions = new ArrayList<Double>();
-        this.customerName = customerName;
-        this.startingBalance=startingBalance;
+        this.name = customerName;
         transactions.add(startingBalance);
-        //where does the branch name fit into all this?
     }
 
-
-    public static Customer createCustomer(String name, double startingBalance
-           ) {
-        ArrayList<Double> transactions = new ArrayList<>();
-        transactions.add(startingBalance);
-        return new Customer(name, startingBalance);
-    }
-
-    public String getCustomerName() {
-        return customerName;
+    public String getName() {
+        return name;
     }
 
 
@@ -38,15 +28,15 @@ public class Customer {
         return transactions;
     }
 
-    public double addTransaction(double deposit){
-        //already verified indentity in the overloaded version.
-//        int index = transactions.size();
-        double last = (transactions.get(transactions.size()-1));
+    public void addTransaction(double deposit){
+        //already verified identity in the overloaded version.
+        int index = (transactions.size()-1);
+        double last = (transactions.get(index));
         double x = deposit+last;
         transactions.add(x);
-        System.out.println("Deposit of " + deposit + " has been made. Your " +
-                "new total is  " + x);
-        return x;
+        System.out.println("Deposit of " + df.format(deposit) + " has been " +
+                "made. Your " +
+                "new total is " + df.format(x) + ".");
     }
 
 
